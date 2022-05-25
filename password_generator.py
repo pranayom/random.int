762d7ea6f8f84d2bc5c98560741557cb9d2e4e38
Pranay Tiwari <pranayom@users.noreply.github.com>
2022-05-24 21:55:37 -0400

    Password generator
    Generates a random password from an input of number of letters numbers and symbols

---
+++ password_generator.py
@@ -0,0 +1,20 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+rand_letters = random.sample(letters,nr_letters) #sample randomizes without replacement
+rand_symbols = random.sample(symbols, nr_symbols)
+rand_num = random.sample(numbers,nr_numbers)
+
+rand_total = rand_letters + rand_symbols + rand_num
+rand_total = random.sample(rand_total, len(rand_total))
+password = "".join(rand_total)
+print(password)
